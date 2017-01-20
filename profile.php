2fa30d7d84fc0dc21807ea2a45e734c42814ca14
Rohit Agarwala <rohiit95agarwal@gmail.com>
2017-01-21 01:19:51 +0530

    Minor Changes

--- profile.php
+++ profile.php
@@ -6,7 +6,7 @@
         <link rel="stylesheet" href="style/addAccordion.css">
         <link rel="stylesheet" href="style/profile.css" type="text/css">
         <script src="//code.jquery.com/jquery-3.1.1.min.js"></script>
-        <script src="addAccordion.js"></script>
+        <script src="js/addAccordion.js"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
         <script > 
         $(document).ready(function(){
@@ -32,23 +32,33 @@
             </div>
             <div id="prof2">
 
-            <div data-addui='accordion' data-change="hover">
+                <div data-addui='accordion' data-change="hover">
 
-              <div role='header'>
-                    <div id="left">Rape Complaint</div>
-                    <div id="right"><b>Complaint No-</b> #gs4548reg4 <br><b>Date-</b>25/01/2017</div>
-              </div>
-              <div role='content'><p>Name- fjenkfw<br> Location- rggrnjnr<br>gwgugafuw<br>.ehfgejb<br></p>
-                    <div class="connect"><button value="">Chat with Officer</button><button class="call">Video-Call Officer</button></div> 
-              </div>
-              <div role='header'><h3>Rape Complaint</h3><br><b>Complaint No-</b> #gs4548reg4 <br><b>Date-</b>25/01/2017</div>
-                            <div role='content'><p>Name- fjenkfw<br> Location- rggrnjnr<br>gwgugafuw<br>.ehfgejb<br></p>
-              <div class="connect"><button value="">Chat with Officer</button><button class="call">Video-Call Officer</button></div> </div>
-               <div role='header'><h3>Rape Complaint</h3><br><b>Complaint No-</b> #gs4548reg4 <br><b>Date-</b>25/01/2017</div>
-                            <div role='content'><p>Name- fjenkfw<br> Location- rggrnjnr<br>gwgugafuw<br>.ehfgejb<br></p>
-              <div class="connect"><button value="">Chat with Officer</button><button class="call">Video-Call Officer</button></div> </div>
-            </div>
-
+                  <div role='header'>
+                        <div class="left">Rape Complaint</div>
+                        <div class="right"><b>Complaint No-</b> #gs4548reg4 <br><b>Date-</b>25/01/2017</div>
+                  </div>
+                  <div role='content'>
+                        <p>Name- fjenkfw<br> Location- rggrnjnr<br>gwgugafuw<br>.ehfgejb<br></p>
+                        <div class="connect"><button value="">Chat with Officer</button><button class="call">Video-Call Officer</button></div> 
+                  </div>
+                  <div role='header'>
+                        <div class="left">Rape Complaint</div>
+                        <div class="right"><b>Complaint No-</b> #gs4548reg4 <br><b>Date-</b>25/01/2017</div>
+                  </div>
+                  <div role='content'>
+                        <p>Name- fjenkfw<br> Location- rggrnjnr<br>gwgugafuw<br>.ehfgejb<br></p>
+                        <div class="connect"><button value="">Chat with Officer</button><button class="call">Video-Call Officer</button></div> 
+                  </div>
+                  <div role='header'>
+                        <div class="left">Rape Complaint</div>
+                        <div class="right"><b>Complaint No-</b> #gs4548reg4 <br><b>Date-</b>25/01/2017</div>
+                  </div>
+                  <div role='content'>
+                        <p>Name- fjenkfw<br> Location- rggrnjnr<br>gwgugafuw<br>.ehfgejb<br></p>
+                        <div class="connect"><button value="">Chat with Officer</button><button class="call">Video-Call Officer</button></div> 
+                  </div>
+                </div>
             </div>
             <div id="video">
                 <button id="X" onclick=""> X</button>
