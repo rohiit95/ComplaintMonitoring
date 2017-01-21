72aba42bed46609bbd2cb9956b18332219629652
Rohit Agarwala <rohiit95agarwal@gmail.com>
2017-01-21 08:29:26 +0530

    Police Station Profile

--- police_station.php
+++ police_station.php
@@ -1,83 +1,23 @@
-<?php 
-?> 
 <html> 
 <head> 
 	<title> Police Station </title>
-	<style>
-		#stations {
-			font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
-			border-collapse: collapse;
-			width: 100%;
-		}
-
-		#stations td, #stations th {
-			border: 1px solid #ddd;
-			padding: 8px;
-		}
-
-		#stations tr:nth-child(even){background-color: #f2f2f2;}
-
-		#stations tr:hover {background-color: #ddd;}
-
-		#stations th {
-			padding-top: 12px;
-			padding-bottom: 12px;
-			text-align: left;
-			background-color: #4CAF50;
-			color: white;
-		}
-		img {
-			margin-left: 35%;
-		}
-		#header {
-			background-color: black;
-			color: white;
-			padding: 10px;
-			text-align: center;
-			font-family: algerian;
-			font-size: 25px;
-			width: 100%;
-		}
-		#strap1 {
-			background-color: black;
-			color: white;
-			position: absolute;
-			top: 10%;
-			left: 5%;
-			width: 150px;
-			height: 100px;
-			font-family: arial;
-			font-size: 20px;
-			text-align: center;
-		}
-		#strap2 {
-			background-color: black;
-			color: white;
-			position: absolute;
-			top: 10%;
-			right: 5%;
-			width: 150px;
-			height: 100px;
-			font-family: arial;
-			font-size: 20px;
-			text-align: center;
-		}
-		#complaints {
-			font-size: 40px;
-			font-weight: bold;
-		}
-	</style>
+	<link rel="stylesheet" type="text/css" href="style/police_station.css" />
 </head> 
 <body> 
 	<div id="header"> Behala Police Station </div> 
+	<div id="map">
 	<img src="img/maps/behala.png" height="400" width="400" alt="Behala">
-	<div id="strap1"> COMPLAINTS REGISTERED <div id="complaints"> 18 </div> </div>
-	<div id="strap2"> COMPLAINTS PENDING  <div id="complaints"> 12 </div> </div>
+	<br/><a href="img/maps/behala.png"> View Full Map </a>
+	</div>
+	<div id="station_details">
+		<div class="strap"> COMPLAINTS REGISTERED <div id="registered"> 18 </div> </div>
+		<div class="strap"> COMPLAINTS PENDING  <div id="pending"> 12 </div> </div>
+		<div class="rating"> Rating <br/><div id="rate"> <p>3</p><img src="images/star1.png"/><img src="images/star1.png"/><img src="images/star1.png"/><img src="images/star1.png"/><img src="images/star1.png"/> </div> </div>
+	</div>
+	<div id="details">
+	<h2><u>Police Station Details</u></h2>
 	<table id="stations"> 
-		<tr> 
-			<td colspan="2"> Police Station Details </td> 
-			<td> <a href="img/maps/behala.png"> View Full Map </a> </td>			
-		</tr>
+	
 		<tr> 
 			<td colspan="2"> Address </td> 
 			<td> 12, MG Road, Behala, Kolkata - 700106 </td> 
@@ -103,5 +43,6 @@
 			<td> 12 </td> 
 		<tr>
 	</table> 
+	</div>
 </body> 
 </html> 
