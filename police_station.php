2858e188c5ca2bcb1830d0376e7ced62311ddca2
Soham Satyadharma <Soham Satyadharma>
2017-01-21 19:08:42 +0530

    Message:

--- police_station.php
+++ police_station.php
@@ -12,34 +12,35 @@
 	<div id="station_details">
 		<div class="strap"> COMPLAINTS REGISTERED <div id="registered"> 18 </div> </div>
 		<div class="strap"> COMPLAINTS PENDING  <div id="pending"> 12 </div> </div>
-		<div class="rating"> Rating <br/><div id="rate"> <p>3</p><img src="images/star1.png"/><img src="images/star1.png"/><img src="images/star1.png"/><img src="images/star1.png"/><img src="images/star1.png"/> </div> </div>
+		<div class="rating"> Rating <br/><div id="rate"> <p>3</p><img src="images/star2.png"/><img src="images/star2.png"/><img src="images/star2.png"/><img src="images/star1.png"/><img src="images/star1.png"/> </div> </div>
 	</div>
 	<div id="details">
-	<h2><u>Police Station Details</u></h2>
-	<table id="stations"> 
-	
+	<table id="stations" cellspacing="10" > 
 		<tr> 
-			<td colspan="2"> Address </td> 
+			<th colspan="2"> <h2> Police Station Details </h2> </th>  
+		<tr>
+		<tr> 
+			<td> Address </td> 
 			<td> 12, MG Road, Behala, Kolkata - 700106 </td> 
 		<tr>
 		<tr> 
-			<td colspan="2"> Division </td> 
+			<td> Division </td> 
 			<td> South East Division </td> 
 		<tr>
 		<tr> 
-			<td colspan="2"> Officer In Charge </td> 
+			<td> Officer In Charge </td> 
 			<td> Shri A B Chattopadhyay </td> 
 		<tr>
 		<tr> 
-			<td colspan="2"> Phone No </td> 
+			<td> Phone No </td> 
 			<td> 2428-7513 </td> 
 		<tr>
 		<tr> 
-			<td colspan="2"> No. of Complaints registered </td> 
+			<td> No. of Complaints registered </td> 
 			<td> 18 </td> 
 		<tr>
 		<tr> 
-			<td colspan="2"> No. of Complaints pending </td> 
+			<td> No. of Complaints pending </td> 
 			<td> 12 </td> 
 		<tr>
 	</table> 
